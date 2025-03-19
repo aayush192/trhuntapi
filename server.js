e4ece973545a8d27ea69c83895912b9a4854e777
Sandesh Khadka <47804440+sandeshkhadka283@users.noreply.github.com>
2025-03-19 13:21:42 +0545

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors"); // Importing the cors package
 
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // For parsing application/json
 
 const port = process.env.PORT || 3000;
