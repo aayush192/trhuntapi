445b47f7d61be46ccaf350521059e03cbbb9dcc8
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-31 22:49:27 +0545

    correct attributesv added

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -121,9 +121,9 @@ const submitAnswer = async (req, res) => {
         session.completed = true;
       }
       await session.save();
-      res.status(200).json({ message: "Correct answer!", nextClueIndex: session.currentClueIndex, completed: session.completed });
+      res.status(200).json({ message: "Correct answer!", nextClueIndex: session.currentClueIndex, completed: session.completed,correct:true });
     } else {
-      res.status(400).json({ error: "Incorrect answer. Try again." });
+      res.status(422).json({ error: "Incorrect answer. Try again." });
     }
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
